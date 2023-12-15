2188c679a75adace07095a01fc4b644a069ac50f
nga <ngant12022002@gmail.com>
2023-12-15 12:01:51 +0700

    hh

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { Socket } = require('socket.io');
+const Socket = require('socket.io');
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server)
 const { v4: uuidV4 } = require('uuid');
 const { ExpressPeerServer } = require('peer');
+
 const peerServer = ExpressPeerServer(server, {
   debug: true
 });
